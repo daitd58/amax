205849e6e4bc951b1ef592aa603a5ec3f4ac2862
daitd <neverfrown.95@gmail.com>
2016-11-29 09:21:44 +0700

    update gulp & sass

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var livereload = require('gulp-livereload');
+
+gulp.task('sass', function() {
+    return gulp.src('sass/style.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(rename('custom-style.css'))
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
+});
+
+//Watch task
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
